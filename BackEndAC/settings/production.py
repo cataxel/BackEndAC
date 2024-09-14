f31e55a0086c752962a2be25512e7bfc01421b4b
Axel López <72405344+cataxel@users.noreply.github.com>
2024-09-13 20:19:42 -0600

    Configuracion inicial del proyecto

---
+++ BackEndAC/settings/production.py
@@ -0,0 +1,20 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['backendac.herokuapp.com']
+
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'yourdbname',
+        'USER': 'yourdbuser',
+        'PASSWORD': 'yourdbpassword',
+        'HOST': 'yourdbhost',
+        'PORT': 'yourdbport',
+    }
+}
+
