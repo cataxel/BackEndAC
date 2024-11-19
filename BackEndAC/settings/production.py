666622614e1beb19c9f7e4ddc25ab338b69f422e
Axel López <72405344+cataxel@users.noreply.github.com>
2024-11-18 20:15:03 -0600

    prueba para produccion

--- BackEndAC/settings/production.py
+++ BackEndAC/settings/production.py
@@ -1,20 +1,35 @@
+from os import environ
+
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['backendac.herokuapp.com']
-
+'''
+env = environ.Env()
+environ.Env.read_env()
 
 # Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'yourdbname',
-        'USER': 'yourdbuser',
-        'PASSWORD': 'yourdbpassword',
-        'HOST': 'yourdbhost',
-        'PORT': 'yourdbport',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
-
+'''
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.nuodkeftaidvvoaikgbt',
+        'PASSWORD': 'AC-D@tabase132',
+        'HOST': 'aws-0-us-east-1.pooler.supabase.com',
+        'PORT': '6543',
+    }
+}
